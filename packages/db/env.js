fab16049a3dc110dcc16e3c2027c1975052adea2
Josh Worden <josh@parameter1.com>
2021-05-25 14:13:39 -0500

    Update env vars

--- packages/db/env.js
+++ packages/db/env.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const { cleanEnv, makeValidator } = require('envalid');
 
-const mongoDsn = makeValidator((str) => `${str}`.includes('mongodb://') && str);
+const validator = makeValidator((str) => `${str}`.includes('mongodb://') && str);
 
 module.exports = cleanEnv(process.env, {
-  MONGO_DSN: mongoDsn({ desc: 'The Base MongoDB connection URL.' }),
+  MONGO_URI: validator({ desc: 'The Base MongoDB connection URL.' }),
 });
