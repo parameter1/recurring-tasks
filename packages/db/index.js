be61371e33a96b5bd60f42a26fa8ac662c7edb45
Josh Worden <josh@parameter1.com>
2021-05-25 14:15:23 -0500

    update db package

--- packages/db/index.js
+++ packages/db/index.js
@@ -1,8 +1,8 @@
 const { createMongoClient, createBaseDB } = require('@parameter1/base-cms-db');
 const { filterDsn } = require('@parameter1/base-cms-db/utils');
-const { MONGO_DSN } = require('./env');
+const { MONGO_URI } = require('./env');
 
-const client = createMongoClient(MONGO_DSN, { appname: '@parameter1/recurring-tasks', useUnifiedTopology: true });
+const client = createMongoClient(MONGO_URI, { appname: '@parameter1/recurring-tasks', useUnifiedTopology: true });
 
 module.exports = {
   client,
